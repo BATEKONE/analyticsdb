c1e5ed1a72948cb11bcadca89bb2e8780c6a3b0c
BATEKONE <svytoslav2002medvedev@gmail.com>
2025-04-15 20:13:09 +0300

    create repo for analyticsdb

---
+++ includes/config.php
@@ -0,0 +1,15 @@
+<?php
+    // $host = "localhost";
+    // $dbname = "analyticsDB";
+    // $user = "postgres";
+    // $password = "1234";
+
+    // try {
+    //     $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    //     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // } catch (PDOException $e) {
+    //     die("Database error: " . $e->getMessage());
+    // }
+
+    // session_start();
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<?php require "includes\config.php" ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // $db->prepare("SELECT * FROM users")
+    ?>
+</body>
+</html>
